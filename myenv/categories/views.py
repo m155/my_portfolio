a8ba971811f59a29cd91bf5406cccf7cd9262f73
m155@umbc.edu <m155@umbc.edu>
2022-01-29 15:42:27 -0500

    main: Initial changes

---
+++ myenv/categories/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, get_object_or_404
+
+from django.http import HttpResponse
+
+from .models import Category
+
+def index(request):
+	categories = Category.objects.values().all()
+	print('Here i am')
+	print(categories[0]['image'])
+	print("stupid anis")
+	
+	return render(request, 'categories/index.html', {'categories':categories})
+
+def detail(request, category_id):
+	category_detail = get_object_or_404(Category,pk=category_id)
+	return render(request, 'categories/detail.html', {'category':category_detail})
